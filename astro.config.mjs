553bc139a6574635c0e6fb43465e3e6d2ded2f84
Radu Andrei Gorcea <198594982+raduandreigorcea@users.noreply.github.com>
2025-11-15 17:21:35 +0200

    Rebuilt the website in Astro, added automatic color picking, image optimization, fixed some mobile responsiveness issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://lamoo7.github.io',
+	base: '/garen-ult-calculator',
+});
